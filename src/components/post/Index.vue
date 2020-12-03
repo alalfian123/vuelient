6aab2f2e65b0de2a24bcd4cc72ef4ae44d5eabf8
Alfian <mymusic.melody1@gmail.com>
2020-12-03 13:48:38 +0700

    first commit

---
+++ src/components/post/Index.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary">Search</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h4>Posts List</h4>
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <h5 class="card-title">Post Title</h5>
+                        <p class="card-text">Descrition</p>
+                        <a href="#" class="card-link">Edit</a>
+                    </div>
+                </div>
+                <button class="btn btn-sm btn-danger m-3">Remove All</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import PostService from "../../services/PostService";
+
+export default {
+    name: "post-list",
+
+    data (){
+        return {
+            posts: []
+        }
+    },
+
+    methods: {
+        retrievePosts() {
+            PostService.getAll()
+            .then((result) => {
+                this.posts = result.data
+                console.log(this.posts);
+                
+            }).catch((err) => {
+                console.log(err);
+            });
+        },
+    },
+
+    mounted(){
+        this.retrievePosts();
+    }
+}
+</script>
